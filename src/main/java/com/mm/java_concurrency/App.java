538286511c86c59a0a10271a50ab4cfd57c93f1f
Manish Chopra <manishrc@hotmail.com>
2013-11-06 11:46:23 -0500

    Add We can do this to the message

--- src/main/java/com/mm/java_concurrency/App.java
+++ src/main/java/com/mm/java_concurrency/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Marlo and Manish!" );
+        System.out.println( "Hello Marlo and Manish! We can totally do this" );
     }
 }
